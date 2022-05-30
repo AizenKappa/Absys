196627461b74809edbe79287fa999a5c42083183
vanix99 <hakimassim2015@gmail.com>
2022-05-30 20:06:11 +0200

    login form

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,10 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>Absys</title>
     @livewireStyles
 </head>
-<body>
+<body class="h-screen">
 
     {{$slot}}
 
