b9472f580832037140800f33e513be9050737596
vanix99 <hakimassim2015@gmail.com>
2022-06-01 11:35:54 +0200

    login-button

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -3,7 +3,7 @@
             w-full
             px-6
             py-2.5
-            bg-blue-600
+            bg-gray-800
             text-white
             font-medium
             text-xs
@@ -11,9 +11,9 @@
             uppercase
             rounded
             shadow-md
-            hover:bg-blue-700 hover:shadow-lg
-            focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-            active:bg-blue-800 active:shadow-lg
+            hover:bg-black-800 hover:shadow-lg
+            focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0
+            active:bg-gray-700 active:shadow-lg
             transition
             duration-150
             ease-in-out">Sign in</button>
--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -41,9 +41,7 @@
           </div>
           <div class="flex justify-between items-center mb-6">
             <div class="form-group form-check">
-              <input type="checkbox" name="check"
-                class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                id="Check">
+              <input type="checkbox" name="check" id="Check">
               <label class="form-check-label inline-block text-gray-800" for="Check">Remember me</label>
             </div>
             <a href="/user_info"
@@ -58,7 +56,7 @@
           @if ($errors->any())
             @foreach ($errors->all() as $error)
               <div class="w-full text-center bg-red-100 py-2 mt-3 rounded-md text-md text-red-500">
-                 {{$error}}              
+                 {{$error}}            
                </div>
             @endforeach
           @endif
